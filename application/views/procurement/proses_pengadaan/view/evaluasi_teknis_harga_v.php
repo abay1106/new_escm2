fe249d9301eb3188758b2e902595d0c444009705
Abdul Hafizh <abdulha05518@gmail.com>
2022-01-13 14:59:50 +0700

    nego dan penawaran

--- application/views/procurement/proses_pengadaan/view/evaluasi_teknis_harga_v.php
+++ application/views/procurement/proses_pengadaan/view/evaluasi_teknis_harga_v.php
@@ -1,59 +1,58 @@
 <div class="row">
-   <div class="col-lg-12">
-      <div class="ibox float-e-margins">
-         <div class="ibox-title">
-            <h5>EVALUASI TEKNIS &amp; HARGA</h5>
-            <div class="ibox-tools">
-               <a class="collapse-link">
-                  <i class="fa fa-chevron-up"></i>
-               </a>
-            </div>
+   <div class="col-12">
+      <div class="card">
+         
+         <div class="card-header border-bottom pb-2">
+            <h4 class="card-title">EVALUASI TEKNIS &amp; HARGA</h4>
          </div>
 
-         <div class="ibox-content">
-            <?php if ($activity_id >= 1140) { ?>
-            <div class="row">
-               <div class="col-md-12">
-                  <a href="<?php site_url();?>index.php/procurement/pdf_bakp/<?php echo $this->session->userdata('rfq_id'); ?>"
-                     type="button" class="btn btn-sm pull-right" data-toggle="tooltip"
-                     title="Uskep Online" target="_blank"
-                     style="background-color:red;color:white;margin-right:5px">
-                     <i class="fa fa-file-pdf-o"></i> USKEP ONLINE
-                  </a>
+         <div class="card-content">
+            <div class="card-body">
+               <?php if ($activity_id >= 1140) { ?>
+               <div class="row">
+                  <div class="col-md-12">
+                     <a href="<?php site_url();?>index.php/procurement/pdf_bakp/<?php echo $this->session->userdata('rfq_id'); ?>"
+                        type="button" class="btn btn-sm pull-right" data-toggle="tooltip"
+                        title="Uskep Online" target="_blank"
+                        style="background-color:red;color:white;margin-right:5px">
+                        <i class="fa fa-file-pdf-o"></i> USKEP ONLINE
+                     </a>
+                  </div>
+               </div>
+               <?php } ?>
+               <div class="table-responsive" style="margin-top:10px">
+                  <table class="table table-bordered" id="evaluasi_teknis_harga_table">
+                     <thead>
+                        <tr>
+                           <th rowspan="2">#</th>
+                           <th rowspan="2">Nilai Total</th>
+                           <th rowspan="2">Nama Vendor</th>
+                           <th rowspan="2">Administrasi</th>
+                           <th colspan="5">Teknis</th>
+                           <th colspan="6">Harga</th>
+                        </tr>
+                        <tr>
+                           <th>Bobot</th>
+                           <th>Nilai</th>
+                           <th>Minimum</th>
+                           <th>Hasil</th>
+                           <th>Catatan</th>
+                           <th>Bobot</th>
+                           <th>Nilai</th>
+                           <th>Hasil</th>
+                           <th>Catatan</th>
+                           <th>Penawaran</th>
+                           <th>Setelah Nego</th>
+                        </tr>
+                     </thead>
+
+                     <tbody></tbody>
+
+                  </table>
                </div>
             </div>
-            <?php } ?>
-            <div class="table-responsive" style="margin-top:10px">
-               <table class="table table-bordered" id="evaluasi_teknis_harga_table">
-                  <thead>
-                     <tr>
-                        <th rowspan="2">#</th>
-                        <th rowspan="2">Nilai Total</th>
-                        <th rowspan="2">Nama Vendor</th>
-                        <th rowspan="2">Administrasi</th>
-                        <th colspan="5">Teknis</th>
-                        <th colspan="6">Harga</th>
-                     </tr>
-                     <tr>
-                        <th>Bobot</th>
-                        <th>Nilai</th>
-                        <th>Minimum</th>
-                        <th>Hasil</th>
-                        <th>Catatan</th>
-                        <th>Bobot</th>
-                        <th>Nilai</th>
-                        <th>Hasil</th>
-                        <th>Catatan</th>
-                        <th>Penawaran</th>
-                        <th>Setelah Nego</th>
-                     </tr>
-                  </thead>
-
-                  <tbody></tbody>
-
-               </table>
-            </div>
          </div>
+
       </div>
    </div>
 </div>
--- application/views/procurement/proses_pengadaan/view/riwayat_eauction_v.php
+++ application/views/procurement/proses_pengadaan/view/riwayat_eauction_v.php
@@ -1,32 +1,24 @@
 <div class="row">
-  <div class="col-lg-12">
-    <div class="ibox float-e-margins">
-      <div class="ibox-title">
-      <h5>Riwayat E-Auction Vendor</h5>
-        <div class="ibox-tools">
-          <a class="collapse-link">
-            <i class="fa fa-chevron-up"></i>
-          </a>
+  <div class="col-12">
+    <div class="card">
+      
+      <div class="card-header border-bottom pb-2">
+          <h4 class="card-title">Riwayat E-Auction Vendor</h4>
+      </div>
 
+      <div class="card-content">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="riwayat_eauction" class="table table-bordered table-striped"></table>
+            </div>
         </div>
       </div>
-      <div class="ibox-content">
-        <div class="table-responsive">
 
-          <table id="riwayat_eauction" class="table table-bordered table-striped"></table>
-
-        </div>
-
-      </div>
     </div>
-
-
   </div>
 </div>
 
 <script type="text/javascript">
-
-
   jQuery.extend({
     getCustomJSON: function(url) {
       var result = null;
