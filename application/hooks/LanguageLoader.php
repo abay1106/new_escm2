a099e45866169ecfd37d3283e85e36fba7fcbc15
labsiard <98461328+labsiard@users.noreply.github.com>
2022-01-29 08:31:15 +0700

    Refactor LanguageLoader Hook

--- application/hooks/LanguageLoader.php
+++ application/hooks/LanguageLoader.php
@@ -6,12 +6,11 @@ class LanguageLoader
        $ci = &get_instance();
        $ci->load->helper('language');
        $siteLang = $ci->session->userdata('site_lang');
-       if ($siteLang) {
-            $ci->lang->load('header',$siteLang);
-            $ci->lang->load('dashboard',$siteLang);
-       } else {
-           $ci->lang->load('header','indonesian');
+       if (!$siteLang) {
+            $siteLang = 'indonesian';
        }
+       $ci->lang->load('header',$siteLang);
+       $ci->lang->load('dashboard',$siteLang);
    }
 
 }
