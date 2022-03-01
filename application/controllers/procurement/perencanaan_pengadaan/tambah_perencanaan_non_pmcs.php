29cb0049dc053367a36a5141834c394cb44f40d1
Joko Indrawan <joko.indrawan@mindasaas.co.id>
2022-03-01 09:41:33 +0700

    tambah data perencanaan non pmcs

--- application/controllers/procurement/perencanaan_pengadaan/tambah_perencanaan_non_pmcs.php
+++ application/controllers/procurement/perencanaan_pengadaan/tambah_perencanaan_non_pmcs.php
@@ -1,7 +1,7 @@
 <?php
   $view = 'procurement/perencanaan_pengadaan/tambah_perencanaan_non_pmcs_v';
 
-	$data['actionPostMatgis'] = base_url()."procurement/perencanaan_pengadaan/perencanaan_non_pmcs/post_create";
+	$data['actionPostMatgis'] = site_url()."/procurement/perencanaan_pengadaan/submit_data_perencanaan_non_pmcs";
 
   $data['curr'] = $this->Procurement_m->get_curr();
 	$data['emp'] = $this->Procurement_m->get_employee();
